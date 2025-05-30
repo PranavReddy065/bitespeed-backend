a5911a59d34d7fdf034b8f7fd0cd83875346a0d9
PranavReddy065 <annempranavreddy@gmail.com>
2025-05-30 18:41:40 +0530

    set up prisma and added base route and controller for contact identification

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import pool from './db';
+import contactRoutes from './routes/contact';  
+
+dotenv.config();
 
 pool.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch((err) => console.error('Failed to connect to PostgreSQL:', err));
 
-
-dotenv.config();
-
 const app = express();
+
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use('/api', contactRoutes);
 
 app.get('/', (_req, res) => {
   res.send('Bitespeed Backend is running!');
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
